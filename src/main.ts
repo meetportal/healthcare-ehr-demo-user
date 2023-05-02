142655477b66d76e2e7d5d3c2e6f92a30151e7a8
Rob Taylor <roboncode@gmail.com>
2023-05-01 19:06:41 -0600

    fetch ehr user

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
-import { WINDOW, usePortalService } from '@meetportal/portal-js'
+import { STORE, usePortalService } from '@meetportal/portal-js'
 
 (async function () {
   try {
     const portal = usePortalService()
-  
-    const response = await portal.echo('Hello, world!')
-    console.log(response)
-  
-    const off = portal.subscribe(WINDOW.ON_URL_CHANGE, '*', (url: string) => {
-      if (!url) return
-      console.log('URL CHANGED:', url)
-      off()
+
+    portal.onSessionChange(session => {
+      console.log('🍀 session', session)
     })
+
+    const ehrUser = await portal.sendRequest(STORE.GET_LOCAL_STORAGE_ITEM, 'ehrUser')
+    console.log('🍀 ehrUser', ehrUser)
+
+    // if(ehrUser) {
+    //   portal.setUser({
+    //     id: ehrUser.id,
+    //   })
+    // }
+  
+    // const response = await portal.echo('Hello, world!')
+    // console.log(response)
+  
+    // const off = portal.subscribe(WINDOW.ON_URL_CHANGE, '*', (url: string) => {
+    //   if (!url) return
+    //   console.log('URL CHANGED:', url)
+    //   off()
+    // })
   } catch (error: any) {
     console.error(error)
   }
