a8f756f53b8c25c50d9a1881e2e175ed92e269d9
Rob Taylor <roboncode@gmail.com>
2023-05-11 15:01:29 -0600

    updated demo to handle login screen

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
-import { STORE, usePortalService } from '@meetportal/portal-js'
-
+import { STORE, WINDOW, usePortalService } from '@meetportal/portal-js'
+;
 (async function () {
   try {
     const portal = usePortalService()
 
-    const s = await portal.sendRequest(STORE.GET_LOCAL_STORAGE_ITEM, 'ehrUser')
-    if(s) {
-      const ehrUser = JSON.parse(s) as { id: string, name: string}
-      portal.setUser({
-        id: ehrUser.id,
-      })
-    }
+    let ehrUser: any
 
+    portal.subscribe(WINDOW.ON_URL_CHANGE, '', (url: string) => {
+      if (url.includes('login') && ehrUser) {
+        ehrUser = null
+        portal.setUser()
+      } else if (!ehrUser) {
+        portal.sendRequest(STORE.GET_LOCAL_STORAGE_ITEM, 'ehrUser').then(s => {
+          if (s) {
+            ehrUser = JSON.parse(s) as { id: string; name: string }
+            try {
+              if (ehrUser.id) {
+                portal.setUser({
+                  id: ehrUser.id,
+                })
+              }
+            } catch (e) {
+              portal.setUser()
+            }
+          }
+        })
+      }
+    })
   } catch (error: any) {
     console.error(error)
   }
