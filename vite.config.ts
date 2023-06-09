0de557460923219b6ced4a78c7d2c4b525e66964
Rob Taylor <roboncode@gmail.com>
2023-06-09 12:01:05 -0600

    added version

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "./src/main.ts",
-      name: "worker",
-      fileName: () => "worker.js",
+      entry: './src/main.ts',
+      name: 'worker',
+      fileName: () => 'worker.js',
       formats: ['iife'],
-    }
-  }
-});
+    },
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
+})
